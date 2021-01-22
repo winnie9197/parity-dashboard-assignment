a295123c5e0641abf8ab88e8a4c4c25ae76657dd
winniehcy <winniehcyeung@gmail.com>
2021-01-22 02:22:39 -0500

    display currentTemp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import IndoorTemp from './components/indoorTemp';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="boxes-container"> 
+          <div className="boxes">1</div>
+          <div className="boxes">2</div>
+          <div className="boxes filler">3</div>  
+          <div className="boxes">4</div>
+          <div className="boxes"><IndoorTemp /></div>
+          <div className="boxes">6</div>  
+          <div className="boxes">7</div>
+          <div className="boxes filler">8</div>
+          <div className="boxes">9</div>  
+        {/* <table className="boxes-layout">
+          <tbody>
+            <tr>
+              <td><div>Firstname</div></td>
+              <td><div>Firstname</div></td>
+              <td className="filler"><div></div></td>
+            </tr>
+            <tr>
+              <td>Jill</td>
+              <td><IndoorTemp /></td>
+              <td>50</td>
+            </tr>
+            <tr>
+              <td>Eve</td>
+              <td>Jackson</td>
+              <td>94</td>
+            </tr>
+          </tbody>
+        </table> */}
+      </div>
     </div>
   );
 }
---
+++ src/components/indoorTemp.js
@@ -0,0 +1,96 @@
+import { useEffect, useState } from 'react';
+
+function IndoorTemp () {
+    //Change: get indoor data => 
+    const [ startTemp, setStartTemp ] = useState(25);
+    const [ sensors, setSensors ] = useState([]);
+
+    const fetchSensors = () => {
+        const sensorUrl = 'http://api-staging.paritygo.com/sensors/api/sensors/';
+        
+        fetch(sensorUrl, {
+            method: 'GET',
+        }).then(response => response.json())
+        .then(data => {
+            console.log('Success:', data);
+            
+            setSensors(data);
+
+            fetchTemperature(data);
+        }).catch(error => {
+          console.error('Error:', error);
+        });
+    }
+
+    const fetchTemperature = (sensorData) => {
+        const endDate = new Date();
+        const startDate = new Date(endDate);
+        startDate.setMinutes(endDate.getMinutes() - 15);
+
+        const sensorSlug = sensorData[1].slug;
+
+        const temperatureUrl = `http://api-staging.paritygo.com/sensors/api/sensors/${sensorSlug}/?begin=${startDate.toISOString()}&end=${endDate.toISOString()}`
+        fetch(temperatureUrl, {
+            method: 'GET',
+        }).then(response => response.json())
+        .then(d => {
+            // !!! Is there anything we could do if there are less than 3 datapoints?
+            if (d.data_points && d.data_points.length >= 3) {
+                const len = d.data_points.length;
+
+                // console.log(d.data_points);
+
+                var sum = 0;
+
+                for (var i=0; i<len; i++) {
+                    sum += parseFloat(d.data_points[i].value);
+                }
+
+                console.log(Math.round((sum/ len)* 10) / 10);
+                
+                const currentTemp = Math.round((sum/ len)* 10) / 10;
+                setStartTemp(currentTemp);
+                
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+    }
+
+    useEffect(() => {
+        fetchSensors();
+    },[]);
+
+    useEffect(() => {
+        console.log(sensors, "senros");
+     }, [sensors]);
+
+    function handleIncrement() {
+        setStartTemp(startTemp+0.5);
+
+        // update actual thermometer
+    }
+
+    function handleDecrement() {
+        setStartTemp(startTemp-0.5);
+
+        // update actual thermometer
+    }
+
+    return (
+        <div className="container-center">
+            <div className="indoorTemp">
+                <h1 className="startTemp">{startTemp}&#176;C</h1>
+                <div>
+                    <button onClick={handleIncrement}>+0.5</button>
+                    <button onClick={handleDecrement}>-0.5</button>
+                </div>
+                <h5 className="sub-text">INDOOR TEMPERTURE</h5>
+            </div>
+        </div>
+    );
+};
+
+
+export default IndoorTemp;
